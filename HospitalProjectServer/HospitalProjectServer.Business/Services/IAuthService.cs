1062339da267e24db3a3d77c16351906c4137a04
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-11-21 15:55:28 +0300

    .

--- HospitalProjectServer/HospitalProjectServer.Business/Services/IAuthService.cs
+++ HospitalProjectServer/HospitalProjectServer.Business/Services/IAuthService.cs
@@ -2,6 +2,7 @@
 using TS.Result;
 
 namespace HospitalProjectServer.Business.Services;
+
 public interface IAuthService
 {
     Task<Result<LoginResponseDto>> LoginAsync(LoginRequestDto request,CancellationToken cancellationToken);
