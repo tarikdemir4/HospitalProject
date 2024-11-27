83bc502c4dc2b5c6e16624cc307d4520a24be74c
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-11-27 23:03:50 +0300

    Update IAuthService.cs

--- HospitalProjectServer/HospitalProjectServer.Business/Services/IAuthService.cs
+++ HospitalProjectServer/HospitalProjectServer.Business/Services/IAuthService.cs
@@ -4,7 +4,9 @@ using TS.Result;
 namespace HospitalProjectServer.Business.Services;
 public interface IAuthService
 {
+
     Task<Result<LoginResponseDto>> LoginAsync(LoginRequestDto request, CancellationToken cancellationToken);
     Task<Result<LoginResponseDto>> GetTokenByRefreshTokenAsync(string refreshToken, CancellationToken cancellationToken);
+    
     
 }
