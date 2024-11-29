f634442a0e37b281ed0e6ae77dc87f066ea36071
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-11-30 00:46:46 +0300

    Update IUserService.cs

--- HospitalProjectServer/HospitalProjectServer.Business/Services/IUserService.cs
+++ HospitalProjectServer/HospitalProjectServer.Business/Services/IUserService.cs
@@ -4,6 +4,5 @@ using TS.Result;
 namespace HospitalProjectServer.Business.Services;
 public interface IUserService
 {
-    
     Task<Result<string>>CreateUserAsync(CreateUserDto request, CancellationToken cancellationToken);
 }
