16aacb52112eea4eee2a712a9f58beac39be0ce7
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-12-01 00:58:08 +0300

    Update CreateUserDtoValidator.cs

--- HospitalProjectServer/eHospitalServer.Business/Validators/CreateUserDtoValidator.cs
+++ HospitalProjectServer/eHospitalServer.Business/Validators/CreateUserDtoValidator.cs
@@ -6,6 +6,7 @@ public sealed class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
 {
     public CreateUserDtoValidator()
     {
+        
         RuleFor(p=> p.FirstName).NotEmpty().WithMessage("Ad alanı boş olamaz");
         RuleFor(p=> p.FirstName).NotNull().WithMessage("Ad alanı boş olamaz");
         RuleFor(p=> p.FirstName).MinimumLength(3).WithMessage("Ad alanı 3 karakterden uzun olmalı");
