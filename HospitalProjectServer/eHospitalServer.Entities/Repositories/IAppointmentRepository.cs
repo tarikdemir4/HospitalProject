1bc41c83daacaffa0885ffaf017fe9ed00812124
Tarık DEMİR <143128759+tarikdemir4@users.noreply.github.com>
2024-12-01 23:28:31 +0300

    Update IAppointmentRepository.cs

--- HospitalProjectServer/eHospitalServer.Entities/Repositories/IAppointmentRepository.cs
+++ HospitalProjectServer/eHospitalServer.Entities/Repositories/IAppointmentRepository.cs
@@ -2,6 +2,4 @@
 using GenericRepository;
 
 namespace eHospitalServer.Entities.Repositories;
-public interface IAppointmentRepository : IRepository<Appointment>
-{
-}
+public interface IAppointmentRepository : IRepository<Appointment>{}
